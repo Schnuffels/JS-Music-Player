7cd3256b3607ca61db0b596d5af4813a0912bfc9
幻想蘑菇 <943771598@qq.com>
2019-09-01 11:24:59 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,286 @@
+# MusicPlayerHCW
+一款好用的H5音乐播放器封装类
+
+## 使用方法
+
+> 说明
+
+* 本音乐播放器主要针对 JS封装类 展开描述，不展开 CSS 与 HTML 的讨论  
+* 本项目提供的界面只是给你看清功能，并不追求花哨  
+* 你可以根据以下说明或者下载项目中的 HTML 例子进行参考或者修改
+
+> 创建一个播放器对象
+
+* playerId 为你的 audio 标签的 id 或者 class
+* coverImgId 为你的 img 标签（此标签用作封面图）的 id 或者 class
+
+```javascript
+//创建播放器对象
+var player = new MusicPlayerHCW(playerId,coverImgId,{
+    'volume':0.5,       //初始音量大小（可以不设置）
+    'speed':1,          //初始播放速度（可以不设置）
+    'music':music,      //初始音乐播放路径
+    'photo':photo       //初始封面图
+});
+//使用 jQuery框架 需特别注意，上面初始化播放器的代码须放在 $(function(){}) 外面
+//默认情况下，上面的方法写在js脚本最外层，下面的“调用方法”全部写在 window.onlooad 或者 $(function(){}) 中
+```
+
+
+## 调用方法
+
+这里说明调用方法的规范
+
+>  播放
+
+```javascript
+player.play();
+
+//播放并回调
+player.play(function(){
+  //内容
+});
+```
+
+>  修改曲目并播放
+
+* nextMusic 为要改变的歌曲 src
+* nextPhoto 为要改变的歌曲封面 src
+* change() 使设置生效
+
+```javascript
+player.setMusic(nextMusic).setCoverImage(nextPhoto).change().play();
+```
+>  暂停
+
+```javascript
+//直接调用暂停
+player.pause();
+
+//暂停并回调
+player.pause(function(){
+    //内容
+});
+```
+
+>  停止
+
+```javascript
+//直接调用停止播放
+player.stop();
+
+//停止并回调
+player.stop(function(){
+    //内容
+});
+```
+
+>  获取当前音量
+
+```javascript
+player.getVolume();
+```
+
+>  设置音量大小
+
+* number 为音量大小，阈值为 0.0-1.0
+
+```javascript
+player.setVolume(number);
+```
+
+>  音量逐渐调整（增强/减弱）
+
+```javascript
+//音量增加
+player.volumeUp();
+
+//音量降低
+player.volumeDown();
+```
+
+>  添加一个音量控制条监听器
+
+* className 为你页面上，设计出来控制音量大小的控制条
+
+```javascript
+player.addVolumeProgressEventListener(className);
+```
+
+>  静音
+
+```javascript
+//获取当前静音状态
+player.mute();
+
+//设置静音
+player.mute(true);
+
+//取消静音
+player.mute(false);
+
+//静音  JS外部调用方法
+var muteButton = document.querySelector('.muteButton');
+mutButton.addEventListener('click', function () {
+    if (player.mute()){     //判断当前是否处于静音状态，是就取消静音
+        player.mute(false);
+    }else{
+        player.mute(true);  //静音
+    }
+});
+
+//静音  jQuery外部调用方法
+$('.muteButton').on('click', function () {
+    if (player.mute()){     //判断当前是否处于静音状态，是就取消静音
+        player.mute(false);
+    }else{
+        player.mute(true);  //静音
+    }
+});
+
+```
+
+>  获取当前播放进度百分比
+
+```javascript
+player.getProgress();
+```
+
+>  设置并改变音乐播放进度
+
+* 需要外部给进度条元素单独添加监听器，function 中 传入 event
+* event 鼠标点击元素事件
+* object 监听器监听对象
+
+```javascript
+player.setPlayProgress(event, object);
+
+//JS 外部调用 setPlayProgress
+var processBar = document.querySelector('.processBar');
+processBar.addEventListener('click', function(event){
+   player.setPlayProgress(event, this);   //传入 event 与 this
+});
+
+//jQuery 外部调用 setPlayProgress 
+$('.processBar').on('click', function (event) {
+    player.setPlayProgress(event, this);
+});
+
+/**
+ * 请注意，以上监听器添加完毕后，通过本类方法改动音量大小音量条会实时发生改变。
+ * 如需要通过鼠标点击音量条改变大小，需要你另行设计一个透明元素通过定位方式悬浮在上方，且事件得您自己设计。
+ */
+```
+
+>  添加一个进度条监听器
+
+* className 为你设计的进度条的 class 或者 id
+* startTimeClassName 为开始时间的(div盒子/容器)的 class 或者 id
+* endTimeClassName 为结束时间的(div盒子/容器)的 class 或者 id
+
+```javascript
+player.addProgressEventListener(className, startTimeClassName, endTimeClassName);
+```
+
+>  添加一个缓冲条监听器
+
+* className 为你自行设计的缓冲条元素的 class 或者 id
+
+```javascript
+player.addBufferedProgressEventListener(className);
+```
+
+>  获取音乐播放状态
+
+```javascript
+player.getPlayStatus();
+```
+
+>  添加一个播放状态监听器
+
+* 本监听器仅播放状态发生改变时触发，每改变状态触发1次
+* 本监听器可以监听的情况有如下：  
+1.非用户操作的播放/暂停行为  
+2.部分浏览器自带播放控件可对音乐进行播放/暂停操作  
+3.JS脚本自动控制的播放/暂停操作  
+* 如未添加本监听器，在未知情况下发生的控件操作行为网站往往探测不到，容易出现程序执行bug
+
+```javascript
+player.addPlayStatusEventListener(function () {
+    //如果正在播放则执行
+},function () {
+    //如果无播放则执行
+});
+```
+
+>  获取当前播放速度
+
+```javascript
+player.getSpeed();
+```
+
+>  设置播放速度
+
+* speed 为速度大小，阈值为 0.7-1.5
+
+```javascript
+player.setSpeed(speed);
+```
+
+>  速度逐渐调整（加快/减弱）
+
+```javascript
+//速度增加
+player.speedUp();
+
+//速度降低
+player.speedDown();
+```
+
+>  获取当前播放的音乐地址
+
+* type: relative | absolutely
+* relative 相对路劲、absolutely 绝对路劲
+```javascript
+player.getMusicSrc('relative');
+```
+
+>  因缓冲加载过慢导致播放停止时触发事件
+
+```javascript
+player.setBufferUnexpectedStop(function(){
+    //当播放进度超过缓冲进度时触发
+});
+```
+
+>  清除监听器
+
+```javascript
+player.clearMonitor();
+```
+
+>  清除指定监听器
+```javascript
+//清除“进度条”监听器
+player.getMonitorProgress().clear();
+
+//清除“音量控制条”监听器
+player.getMonitorVolumeProgress().clear();
+
+//清除“缓冲条”监听器
+player.getMonitorBufferedProgress().clear();
+
+//清除“播放状态”监听器
+player.getMonitorPlayStatus().clear();
+```
+
+
+## 总结
+
+* 本播放器封装类提供了多种监听器，建议在播放器对象创建完成后，直接将所有的监听方法实现。具体的例子可以下载整个项目查看 HTML 中怎么去操作。
+* 音乐资源一个来源网络对象存储，一个来源本地。目的是为了观察缓冲条的状态与区别，不过对象存储服务器的加载速度也是非常快的，未必能看得清缓冲效果。
+* 网络的音乐资源并不是一次性加载完成，有可能加载到某一个点停止缓冲，等快播放到的时候再开始缓冲，本地资源缓冲直接100%。
+
+## 使用与测试
+https://www.pokemonhome.cn/  
+音乐版权是个雷区，仅做测试用途，访问密码请找作者本人询问
---
+++ js/MusicPlayerHCW.js
@@ -0,0 +1,404 @@
+/**
+ *  轻旋韵音乐播放器封装类
+ *  QQ:943771598、642476152
+ *  author:HuangCanWen
+ *  Date of creation:2019/08/24
+ *
+ *  使用方法：创建一个播放器对象
+ *  var MusicPlayerHCW = new MusicPlayerHCW(audio播放器对象, img封面图对象, {
+ *    volume:音量大小,
+ *    speed:播放速度,
+ *    music:音乐资源,
+ *    photo:封面图资源
+ *  });
+ */
+(function () {
+    var mediaPlay;
+    var coverImg;
+    var monitor;
+    function MusicPlayerHCW(playerID, coverImgID, obj) {
+        this.player = playerID;
+        this.coverImg = coverImgID;
+        this.volume = obj.volume == null ? 0.5 : obj.volume.toFixed(1);
+        this.speed = obj.speed == null ? 1 : obj.speed.toFixed(1);
+        this.music = obj.music;
+        this.photo = obj.photo;
+        this.progressPercent = '0.00%';
+
+        mediaPlay = document.querySelector(this.player);
+        coverImg = document.querySelector(this.coverImg);
+        if (mediaPlay == null){
+            alert('未找到播放器 audio 标签');
+        }else if(coverImg == null){
+            alert('未找到封面图 img 标签');
+        } else{
+            mediaPlay.setAttribute('src', this.music);
+            this.initCoverImg = coverImg.getAttribute('src');
+            coverImg.setAttribute('src',this.photo);
+
+            if (this.volume > 0.9){
+                this.volume = 1; mediaPlay.volume = 1;
+            }else if (this.volume <= 0.1){
+                this.volume = 0; mediaPlay.volume = 0;
+            }else {
+                mediaPlay.volume = this.volume;
+            }
+            if (this.speed >= 1.5){
+                this.speed = 1.5; mediaPlay.playbackRate = 1.5;
+            }else if (this.speed <= 0.7){
+                this.speed = 0.7; mediaPlay.playbackRate = 0.7;
+            }else{
+                mediaPlay.playbackRate = this.speed;
+            }
+        }
+    }
+
+    //获取音乐链接
+    MusicPlayerHCW.prototype.getMusicSrc = function (type) {
+        switch (type){
+            case 'relative':
+                return this.music;
+            case 'absolutely':
+                return mediaPlay.currentSrc;
+            default:
+                console.log('传入类型错误：分别为 relative 与 absolutely');
+                return 'error:getMusicSrc(type)'
+        }
+    };
+
+    //获取播放速度
+    MusicPlayerHCW.prototype.getSpeed = function () {
+        return mediaPlay.playbackRate;
+    };
+
+    //设置播放速度
+    MusicPlayerHCW.prototype.setSpeed = function (speed) {
+        if (speed >= 1.5){
+            this.speed = 1.5;
+            mediaPlay.playbackRate = 1.5;
+        }else if (speed <= 0.7){
+            this.speed = 0.7;
+            mediaPlay.playbackRate = 0.7;
+        }else{
+            this.speed = speed;
+            mediaPlay.playbackRate = speed;
+        }
+        return this;
+    };
+
+    //增加播放速度
+    MusicPlayerHCW.prototype.speedUp = function () {
+        if (mediaPlay.playbackRate > 1.4){
+            mediaPlay.playbackRate = 1.5;
+            this.speed = 1.5;
+        }else{
+            mediaPlay.playbackRate = (mediaPlay.playbackRate + 0.1).toFixed(1);
+            this.speed = (mediaPlay.playbackRate).toFixed(1);
+        }
+    };
+
+    //减小播放速度
+    MusicPlayerHCW.prototype.speedDown = function () {
+        if (mediaPlay.playbackRate < 0.8){
+            mediaPlay.playbackRate = 0.7;
+            this.speed = 0.7;
+        }else{
+            mediaPlay.playbackRate = (mediaPlay.playbackRate - 0.1).toFixed(1);
+            this.speed = (mediaPlay.playbackRate).toFixed(1);
+        }
+    };
+
+    //设置播放曲目
+    MusicPlayerHCW.prototype.setMusic = function(musicSrc) {
+        this.music = musicSrc;
+        this.progressPercent = '0.00%';
+        return this;
+    };
+
+    //设置封面图片
+    MusicPlayerHCW.prototype.setCoverImage = function (photoSrc) {
+        this.photo = photoSrc;
+        return this;
+    };
+
+    //使改变的曲目在播放器中生效
+    MusicPlayerHCW.prototype.change = function () {
+        mediaPlay.setAttribute('src',this.music);
+        coverImg.setAttribute('src',this.photo);
+        mediaPlay.playbackRate = this.speed;
+        endTimeChange = true;
+        return this;
+    };
+
+    //播放
+    MusicPlayerHCW.prototype.play = function (callback) {
+        var tempMonitor = window.setInterval(function () {
+            if (mediaPlay.getAttribute('src').toString() !== ''){
+                mediaPlay.play();
+                window.clearInterval(tempMonitor);
+            }
+        },100);
+
+        if (callback){callback();}
+        return this;
+    };
+
+    //设置播放完成后执行的动作 (自定义传参参数，回调函数)
+    MusicPlayerHCW.prototype.setActionsAfterPlayback = function (object, callback) {
+        mediaPlay.addEventListener('ended',function () {
+            if (callback){callback(object)}
+        });
+    };
+
+    //暂停
+    MusicPlayerHCW.prototype.pause = function (callback) {
+        mediaPlay.pause();
+        if (callback){callback();}
+    };
+
+    //停止
+    MusicPlayerHCW.prototype.stop = function (callback) {
+        mediaPlay.pause();
+        mediaPlay.currentTime = 0;
+        mediaPlay.setAttribute('src', '');
+        coverImg.setAttribute('src', this.initCoverImg);
+        if (callback){callback();}
+    };
+
+    //获取音乐播放状态
+    MusicPlayerHCW.prototype.getPlayStatus = function () {
+        return !mediaPlay.paused;
+    };
+
+    //监听音乐播放状态发生改变时执行的结果（回调1：正在播放，回调2：无播放）
+    var playStatusMonitor;
+    MusicPlayerHCW.prototype.addPlayStatusEventListener = function (callback1, callback2) {
+        var implementState = false;         //控制回调执行
+        var timerState = true;              //放开闸口1次
+        playStatusMonitor = window.setInterval(function () {
+            //闸口
+            implementState = mediaPlay.paused == timerState ? false : true;
+            //播放与暂停状态
+            if (implementState){
+                timerState = !timerState;
+                implementState = false;
+                if (!mediaPlay.paused){
+                    if (callback1){callback1()}
+                }else{
+                    if (callback2){callback2()}
+                }
+            }
+        },300);
+    };
+
+    //声音调整(number为空：返回系统音量值|0.0-1.0：音量阈值)
+    MusicPlayerHCW.prototype.setVolume = function (number) {
+        mediaPlay.volume = number;
+        this.volume = number;
+        return this;
+    };
+
+    //获取系统音量值
+    MusicPlayerHCW.prototype.getVolume = function () {
+        return this.volume;
+    };
+
+    //声音增强
+    MusicPlayerHCW.prototype.volumeUp = function () {
+        if (mediaPlay.volume > 0.9){
+            mediaPlay.volume = 1;
+            this.volume = 1;
+        }else{
+            mediaPlay.volume = (mediaPlay.volume + 0.1).toFixed(1);
+            this.volume = (mediaPlay.volume).toFixed(1);
+        }
+    };
+
+    //声音减弱
+    MusicPlayerHCW.prototype.volumeDown = function () {
+        if (mediaPlay.volume < 0.1){
+            mediaPlay.volume = 0;
+            this.volume = 0;
+
+        }else{
+            mediaPlay.volume = (mediaPlay.volume - 0.1).toFixed(1);
+            this.volume =  (mediaPlay.volume).toFixed(1);
+        }
+    };
+
+    //静音 (isMute为空：返回当前静音状态|true：静音|false：取消静音)
+    MusicPlayerHCW.prototype.mute = function (isMute) {
+        if (isMute){
+            mediaPlay.volume = 0;
+            this.isMute = true;
+        }else if (isMute == false){
+            mediaPlay.volume = this.volume;
+            this.isMute = false;
+        }else if (isMute == null){
+            return this.isMute;
+        }
+    };
+
+    //监听与更新音量进度条 (传入进度条类名)
+    var volumeProgressMonitor;
+    MusicPlayerHCW.prototype.addVolumeProgressEventListener = function (className) {
+        var vProgressObj = document.querySelector(className);
+        var parent = this;
+        volumeProgressMonitor = window.setInterval(function () {
+            vProgressObj.style.width = (parent.volume * 100) + '%'
+        },100);
+    };
+
+    //获取当前进度百分比
+    MusicPlayerHCW.prototype.getProgress = function () {
+        var percent = mediaPlay.currentTime / mediaPlay.duration;
+        this.progressPercent = (percent * 100 ).toFixed(2) + '%';
+        return (percent * 100 ).toFixed(2) + '%';
+    };
+
+    //监听与更新进度条 (传入进度条类名, 开始时间类名, 结束时间类名)
+    var progressMonitor;
+    var endTimeChange = true;
+    MusicPlayerHCW.prototype.addProgressEventListener = function (className, startTimeClassName, endTimeClassName) {
+        var progressObj = document.querySelector(className);
+        var startTime = document.querySelector(startTimeClassName);
+        var endTime = document.querySelector(endTimeClassName);
+        var buStopTimer = 0;
+        var buStopStartTime;
+        //进度条更新
+        progressMonitor = window.setInterval(function () {
+            startTime.innerHTML = formateTime(mediaPlay.currentTime);   //当前时间
+            if (endTimeChange){
+                endTimeChange = false;
+                endTime.innerHTML = formateTime(mediaPlay.duration) === '0NaN:0NaN' ? '00:00' : formateTime(mediaPlay.duration);    //结束时间
+                setTimeout(function () {
+                    if (endTime.innerHTML.toString() === '00:00'){
+                        endTimeChange = true;
+                    }
+                },1000);
+            }
+
+            //判断是否因缓冲问题中断播放
+            if (!mediaPlay.paused && startTime.innerHTML.toString() !== endTime.innerHTML.toString()){
+                if (buStopTimer === 10){
+                    buStopStartTime = startTime.innerHTML.toString();
+                }
+                ++ buStopTimer;
+                if (buStopTimer >= 30){
+                    if (startTime.innerHTML.toString() === buStopStartTime){
+                        buStop();
+                        buStopSwitch = true;
+                    }else{
+                        buStopSwitch = false;
+                    }
+                    buStopTimer = 0;
+                }
+
+            }
+
+            var percent = mediaPlay.currentTime / mediaPlay.duration;
+            progressObj.style.width = (percent * 100).toFixed(2) + '%';
+        },100);
+    };
+
+    //设置播放进度
+    MusicPlayerHCW.prototype.setPlayProgress = function (event, object) {
+        //计算点击位置的百分比
+        var currentValue = event.offsetX / object.offsetWidth;
+        mediaPlay.currentTime = mediaPlay.duration * currentValue;
+    };
+
+    //获取缓冲进度
+    var bufferedMonitor;
+    MusicPlayerHCW.prototype.addBufferedProgressEventListener = function (className) {
+        //进度条更新
+        bufferedMonitor = window.setInterval(function () {
+            var buffered = mediaPlay.buffered, loaded;
+            var bufferedProgressObj = document.querySelector(className);
+            if (buffered.length) {
+                // 获取当前缓冲进度
+                loaded = 100 * buffered.end(0) / mediaPlay.duration;
+                // 渲染缓冲条的样式
+                bufferedProgressObj.style.width = (loaded.toFixed(2)) + '%';
+            }
+        },500);
+    };
+
+    //获取“进度条”监听器
+    MusicPlayerHCW.prototype.getMonitorProgress = function () {
+        monitor = progressMonitor;
+        return this;
+    };
+
+    //获取“音量控制条”监听器
+    MusicPlayerHCW.prototype.getMonitorVolumeProgress = function () {
+        monitor = volumeProgressMonitor;
+        return this;
+    };
+
+    //获取“缓冲条”监听器
+    MusicPlayerHCW.prototype.getMonitorBufferedProgress = function () {
+        monitor = bufferedMonitor;
+        return this;
+    };
+
+    //获取“播放状态”监听器
+    MusicPlayerHCW.prototype.getMonitorPlayStatus = function () {
+        monitor = playStatusMonitor;
+        return this;
+    };
+
+    //清理全部监听器
+    MusicPlayerHCW.prototype.clearMonitor = function () {
+        window.clearInterval(progressMonitor);
+        window.clearInterval(volumeProgressMonitor);
+        window.clearInterval(bufferedMonitor);
+        window.clearInterval(playStatusMonitor);
+    };
+
+    //清除指定监听器
+    MusicPlayerHCW.prototype.clear = function () {
+        window.clearInterval(monitor);
+        monitor = null;
+    };
+
+    //时间转换(格式化时间)
+    function formateTime(time) {
+        if (time > 3600) {
+            var hour = parseInt(time / 3600);
+            var minute = parseInt(time % 3600 / 60);
+            var second = parseInt(time % 3600);
+            hour = hour >= 10 ? hour : '0' + hour;
+            minute = minute >= 10 ? minute : '0' + minute;
+            second = second >= 10 ? second : '0' + second;
+            return hour + ':' + minute + ':' + second;
+        }
+        else {
+            var minute = parseInt(time / 60);
+            var second = parseInt(time % 60);
+            minute = minute >= 10 ? minute : '0' + minute;
+            second = second >= 10 ? second : '0' + second;
+            return minute + ':' + second;
+        }
+    }
+
+    //设置缓冲导致的播放停止事件
+    MusicPlayerHCW.prototype.setBufferUnexpectedStop = function (callback) {
+        if(callback){
+            buStop = function () {
+                if (buStopSwitch){
+                    return;
+                }
+                callback();
+            }
+        }
+    };
+
+    //缓冲导致播放停止
+    var buStopSwitch = false;
+    var buStop = function bufferUnexpectedStop() {
+        return;
+    };
+
+    window.MusicPlayerHCW = MusicPlayerHCW;
+})(window);
